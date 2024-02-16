ea638aa48be7f78bbc87168b54a62a889f8a236a
LatEvgeniy <e.latinin48@gmail.com>
2024-02-16 21:39:31 +0200

    add set coin price, improve session

--- bot_api/tg_bot_api.go
+++ bot_api/tg_bot_api.go
@@ -2,18 +2,25 @@ package botapi
 
 import (
 	"CoinPriceNotifierBot/config"
+	"fmt"
+	"strconv"
 
-	"sync"
+	"strings"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	ON_COMMAND_NAME             = "/on"
+	OFF_COMMAND_NAME            = "/off"
+	SET_COIN_PRICE_COMMAND_NAME = "/set_coin_price"
+)
+
 type BotApi struct {
-	api           *tgbotapi.BotAPI
-	userGoroutine map[int64]chan struct{}
-	mutex         sync.Mutex
+	api         *tgbotapi.BotAPI
+	usersConfig map[int64]*config.UserConfig
 }
 
 func NewBotApi(accessToken string) (*BotApi, error) {
@@ -22,7 +29,7 @@ func NewBotApi(accessToken string) (*BotApi, error) {
 		return nil, err
 	}
 
-	return &BotApi{api: api, userGoroutine: make(map[int64]chan struct{}), mutex: sync.Mutex{}}, nil
+	return &BotApi{api: api, usersConfig: make(map[int64]*config.UserConfig)}, nil
 }
 
 func (b *BotApi) Run() {
@@ -38,14 +45,22 @@ func (b *BotApi) Run() {
 	for {
 		select {
 		case update := <-updates:
-			logrus.Infof("Receive new message from %d: %s", update.Message.Chat.ID, update.Message.Text)
+			logrus.Infof(
+				"Received message: %s from %s with chat id: %d",
+				strings.ReplaceAll(update.Message.Text, "\n", " "),
+				update.Message.From.UserName,
+				update.Message.Chat.ID,
+			)
+
 			switch update.Message.Text {
-			case "/on":
+			case ON_COMMAND_NAME:
 				go b.executeOnCommand(update.Message)
-			case "/off":
+			case OFF_COMMAND_NAME:
 				go b.executeOffCommand(update.Message)
+			case SET_COIN_PRICE_COMMAND_NAME:
+				go b.executeSetCoinPriceCommand(update.Message)
 			default:
-				go b.api.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Unknown command: "+update.Message.Text))
+				go b.executeDefaultMsgText(update.Message)
 			}
 		default:
 			time.Sleep(time.Millisecond * 500)
@@ -53,36 +68,94 @@ func (b *BotApi) Run() {
 	}
 }
 
-func (b *BotApi) executeOnCommand(msg *tgbotapi.Message) {
-	stopCh := make(chan struct{})
-
-	// add check unique id
-	b.mutex.Lock()
-	b.userGoroutine[msg.Chat.ID] = stopCh
-	b.mutex.Unlock()
-
-	defaultConfig := config.GetDefaultConfig()
-
+func (b *BotApi) runUserGoroutine(userChatId int64) {
 	for {
 		select {
-		case <-stopCh:
-			logrus.Debugln("Got stop msg")
+		case <-b.usersConfig[userChatId].GoroutineCh:
 			return
 		default:
-			logrus.Debugln(b.userGoroutine)
-			b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "Executing..."))
-			time.Sleep(time.Second * time.Duration(defaultConfig.Timeout))
+			if b.usersConfig[userChatId].ChoosenCommand == "" {
+				b.api.Send(tgbotapi.NewMessage(userChatId, fmt.Sprintf("Your coin price is: %f", b.usersConfig[userChatId].CoinPrice)))
+			}
+			time.Sleep(time.Second * time.Duration(b.usersConfig[userChatId].Timeout))
 		}
 	}
 }
 
+func (b *BotApi) executeDefaultMsgText(msg *tgbotapi.Message) {
+	userConfig, exists := b.usersConfig[msg.Chat.ID]
+
+	if exists {
+		switch userConfig.ChoosenCommand {
+		case SET_COIN_PRICE_COMMAND_NAME:
+			b.executeSetCoinPriceValue(msg)
+			return
+		}
+	}
+	b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "Unknown command: "+strings.ReplaceAll(msg.Text, "\n", " ")))
+}
+
+func (b *BotApi) executeOnCommand(msg *tgbotapi.Message) {
+	userConfig, exists := b.usersConfig[msg.Chat.ID]
+	if exists && userConfig.HasActiveSession {
+		b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "Session already exists, to run new session - stop the previous one"))
+		return
+	}
+
+	if !exists {
+		config := config.GetDefaultConfig()
+		config.GoroutineCh = make(chan struct{})
+
+		b.usersConfig[msg.Chat.ID] = config
+	}
+	b.usersConfig[msg.Chat.ID].HasActiveSession = true
+
+	b.runUserGoroutine(msg.Chat.ID)
+}
+
 func (b *BotApi) executeOffCommand(msg *tgbotapi.Message) {
-	stopCh, ok := b.userGoroutine[msg.Chat.ID]
-	if ok {
-		logrus.Debugln("Sending stop msg")
-		close(stopCh)
-		delete(b.userGoroutine, msg.Chat.ID)
-		logrus.Debugln("Sent stop msg")
+	userConfig, exists := b.usersConfig[msg.Chat.ID]
+	if exists && userConfig.HasActiveSession {
+		stopCh := userConfig.GoroutineCh
+		stopCh <- struct{}{}
+
+		userConfig.HasActiveSession = false
+		b.usersConfig[msg.Chat.ID] = userConfig
+
+		b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "Successfully stop session"))
+		return
+	}
+	b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "No active session"))
+}
+
+func (b *BotApi) executeSetCoinPriceCommand(msg *tgbotapi.Message) {
+	userConfig, exists := b.usersConfig[msg.Chat.ID]
+	if exists && userConfig.HasActiveSession {
+		userConfig.ChoosenCommand = SET_COIN_PRICE_COMMAND_NAME
+
+		b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "Send float price value"))
+		return
+	}
+	b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "No active session"))
+}
+
+func (b *BotApi) executeSetCoinPriceValue(msg *tgbotapi.Message) {
+	userConfig, exists := b.usersConfig[msg.Chat.ID]
+	if exists && userConfig.HasActiveSession {
+		floatPrice, err := strconv.ParseFloat(msg.Text, 64)
+		if err != nil {
+			b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("Cannot parse %s to float", msg.Text)))
+			userConfig.ChoosenCommand = ""
+			return
+		}
+		userConfig.CoinPrice = floatPrice
+		userConfig.ChoosenCommand = ""
+
+		stopCh := userConfig.GoroutineCh
+		stopCh <- struct{}{}
+
+		go b.runUserGoroutine(msg.Chat.ID)
+		b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("Successfully changed coin price to: %f", floatPrice)))
 		return
 	}
 	b.api.Send(tgbotapi.NewMessage(msg.Chat.ID, "No active session"))
--- config/user_config.go
+++ config/user_config.go
@@ -3,9 +3,12 @@ package config
 type UserConfig struct {
 	Timeout            int
 	NotificationFormat string
-	UserCoinPrice      int
+	CoinPrice          float64
+	GoroutineCh        chan struct{}
+	ChoosenCommand     string
+	HasActiveSession   bool
 }
 
 func GetDefaultConfig() *UserConfig {
-	return &UserConfig{Timeout: 1, NotificationFormat: "2006-01-02 15:04:05", UserCoinPrice: 0}
+	return &UserConfig{Timeout: 1, NotificationFormat: "2006-01-02 15:04:05"}
 }
